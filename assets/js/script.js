bbe3d0754d50385370bc777178efb53a6866598e
Minosanty <minosantiago17@gmail.com>
2025-05-29 19:17:55 -0300

    se creo la rama develop y las constantes del JS

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const API_BASE = "https://dragonball-api.com/api/characters";
+const btnBuscar = document.getElementById("btn-buscar");
+const contenedorPadre = document.getElementById("contenedor-data");
+const campoEntrada = document.getElementById("campo-entrada") 
+
+// renderizar personajes
+
+
+
+
