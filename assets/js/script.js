1a6266a4728261848ea921f32b1c939b2687c884
Minosanty <minosantiago17@gmail.com>
2025-05-29 20:11:36 -0300

    se creo la constante de carga de datos la cual lanza error si los datos de la api no son validos

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,18 @@ const API_BASE = "https://dragonball-api.com/api/characters";
 const btnBuscar = document.getElementById("btn-buscar");
 const contenedorPadre = document.getElementById("contenedor-data");
 const campoEntrada = document.getElementById("campo-entrada") 
+const cargarDatos = async (API_BASE) => {
+  try {
+    const response = await fetch(API_BASE);
 
-// renderizar personajes
+    if (!response.ok) {
+      throw new error("Error en la API");
+    }
 
+    const data = await response.json();
 
-
-
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
