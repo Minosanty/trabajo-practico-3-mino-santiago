459a2eaa0cd42b7cc078fe7dfe326716e981013f
Minosanty <minosantiago17@gmail.com>
2025-05-30 01:02:19 -0300

    en el archivo js se agregaron los personajes y en el archivo css se agrego el fondo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://dragonball-api.com/api/characters?limit=100";
+const API_BASE = "https://dragonball-api.com/api/characters?limit=57";
 const btnBuscar = document.getElementById("btnBuscar");
 const contenedorPadre = document.getElementById("contenedordata");
 const campoEntrada = document.getElementById("campoentrada");
@@ -28,3 +28,33 @@ const verDetalles = async (id) => {
     console.log(error);
   }
 };
+
+btnBuscar.addEventListener("click", async () => {
+  contenedorPadre.innerHTML = ""; // Limpia el contenedor
+  const data = await cargarDatos(API_BASE);
+
+  // Verifica si la propiedad correcta es "items"
+  const personajes = data.items || data || []; 
+
+  console.log(personajes);
+
+  personajes.forEach((personaje) => {
+    contenedorPadre.innerHTML += `
+     <div class="col-12 col-sm-6 col-md-4 col-lg-3 pb-3 d-flex justify-content-center">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="${personaje.image}" alt="${personaje.name}" />
+          <div class="card-body">
+            <h5 class="card-title">${personaje.name}</h5>
+            <p class="card-text">
+              <strong>Raza:</strong> ${personaje.race}<br>
+              <strong>Género:</strong> ${personaje.gender}<br> 
+              <strong>Ki:</strong> ${personaje.ki}
+            </p>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+});
+
+
