dc37afecb5b9c3bbb7a19575d4a00de284a29cf5
Minosanty <minosantiago17@gmail.com>
2025-05-30 00:38:19 -0300

    se creo una carpeta y se modifico el archivo  js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,29 @@
-const API_BASE = "https://dragonball-api.com/api/characters";
-const btnBuscar = document.getElementById("btn-buscar");
-const contenedorPadre = document.getElementById("contenedor-data");
-const campoEntrada = document.getElementById("campo-entrada") 
-const cargarDatos = async (API_BASE) => {
+const API_BASE = "https://dragonball-api.com/api/characters?limit=100";
+const btnBuscar = document.getElementById("btnBuscar");
+const contenedorPadre = document.getElementById("contenedordata");
+const campoEntrada = document.getElementById("campoentrada");
+
+const cargarDatos = async (url) => {
   try {
-    const response = await fetch(API_BASE);
-
+    const response = await fetch(url);
     if (!response.ok) {
-      throw new error("Error en la API");
+      throw new Error("Error en la API");
     }
-
     const data = await response.json();
-
     return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const verDetalles = async (id) => {
+  try {
+    const response = await fetch(`https://dragonball-api.com/api/characters/${id}`);
+    if(!response.ok) {
+      throw new Error("Error en la API");
+    }
+    const data = await response.json();
+    alert(data.description || "Sin descripción");
   } catch (error) {
     console.log(error);
   }
